eef9b8189a13bd991fc02c2f4dfedb85e6350094
Haritha V <90097766+harithavishva@users.noreply.github.com>
2023-01-09 18:27:42 +0530

    add the theory

---
+++ script.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "Haritha V",
+      "email": "harithasai01@gmail.com",
+      "phone": 8667386035,
+      "degree": "BSC",
+      "location": {
+        "address": "No:17/225 Thendral Nagar 5th st,Pattabiram",
+        "postalCode": 600072,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/haritha-viswanathan-355a79182/",
+          "github":"https://github.com/harithavishva"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Cognizant Techonolgy Solution Pvt.lmtd",
+        "position": "Senior Process Executive",
+        "startDate": "2019-09-16",
+        "endDate": "2022-12-06",
+        "summary": "i am the senior process executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Madras university",
+        "department": "Computer Applications",
+        "studyType": "part-time",
+        "batch start year": 2019,
+        "batch end year": 2022,
+        "gpa": 8.6,
+      }
+    ],
+    "skills": [
+        {
+          "name": "javascript",
+          "level": "beginner",
+          "project": [
+            "College Website using Javascript language"
+          ]
+        }
+      ],
+      "languages": [
+        {
+          "language": "Tamil,Enlish",
+        }
+      ],
+      "interests": [
+        {
+          "name": "Crafting,Wall Painting,",
+        }
+      ]
+    }
+    console.log(myResume);
